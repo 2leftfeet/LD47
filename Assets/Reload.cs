00e486385a9dbbb7cb333dbb6c4f4d9f04fda433
Justas <justasz@unity3d.com>
2020-10-04 00:37:34 +0300

    enema

---
+++ Assets/Reload.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reload : MonoBehaviour
+{
+
+    public FieldOfView fov;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ReloadQueue(){
+        StartCoroutine("DisableScript");
+    }
+
+    IEnumerator DisableScript()
+    {
+        fov.enabled = false;
+
+        yield return new WaitForSeconds(3f);
+
+        fov.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
