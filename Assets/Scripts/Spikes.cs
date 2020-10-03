805b865d87e2adcadda4652e07dc77538024571e
Arnas Bendinskas <arnas@unity3d.com>
2020-10-03 21:21:47 +0300

    no message

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(OverlapTrigger))]
+public class Spikes : MonoBehaviour
+{
+
+    OverlapTrigger deathZone;
+
+
+    private void Awake()
+    {
+        deathZone.OnTriggerEnter += KillPlayer;
+    }
+
+    void KillPlayer(Collider2D player)
+    {
+        //call a kill on a necesery player
+        GetComponent<CharController>().TriggerDeath();
+    }
+}
