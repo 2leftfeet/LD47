2bad6e00a2575cdf8ba2cef412fc3ed1c2e5e9be
Aivaras Ivoškus <aiva.ivoskus@gmail.com>
2020-10-04 20:50:34 +0300

    Simple audio manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance => instance;
+    private static AudioManager instance;
+
+    private AudioSource audioSource;
+    
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            audioSource = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Debug.LogError($"This is not okie dokie, two {GetType().ToString()} exist");
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static void PlayClip(AudioClip clip)
+    {
+        instance.audioSource.PlayOneShot(clip);
+    }
+
+    public static void PlayMusic()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public enum MusicType { MainTheme, Scene, Credits }
+}
