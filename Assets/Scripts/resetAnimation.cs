3a1486a756706b3e7ca470f12bc117d5485fcf2a
Arnas Bendinskas <arnas@unity3d.com>
2020-10-04 17:04:51 +0300

    level dva bare to test

---
+++ Assets/Scripts/resetAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class resetAnimation : MonoBehaviour
+{
+    PlayableDirector playableDirector;
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        LoopManager.ResetReplay += Reset;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playableDirector = GetComponent<PlayableDirector>();
+    }
+
+    void Reset()
+    {
+        playableDirector.time = 0;
+    }
+}
