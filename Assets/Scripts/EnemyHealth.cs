d4ee68018c490f564184b4d21c793f51463f2980
Liutauras <liutauras@unity3d.com>
2020-10-04 21:39:49 +0300

    Enmy imprvmnts

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,6 +4,36 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    Vector3 startPosition;
+
+    Wander wander;
+    Reload reload;
+
+    void Start()
+    {
+        LoopManager.ResetReplay += ResetEnemy;
+    }
+
+    void OnDestroy()
+    {
+        LoopManager.ResetReplay -= ResetEnemy;
+    }
+
+    void Awake()
+    {
+        startPosition = transform.position;
+        wander = GetComponent<Wander>();
+        reload = GetComponent<Reload>();
+    }
+
+    public void ResetEnemy()
+    {
+        transform.position = startPosition;
+        gameObject.SetActive(true);
+        wander.Reset();
+        reload.Reset();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         var projectile = other.GetComponent<ProjectileMover>();
