5034a6e1ba8c70c92a732854d32a2841195ff29b
Arnas Bendinskas <arnas@unity3d.com>
2020-10-04 20:48:48 +0300

    Blurry mat for end game screen plus a a start on other stuff

---
+++ Assets/Scripts/LevelEndSequence.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(OverlapTrigger))]
+public class LevelEndSequence : MonoBehaviour
+{
+    OverlapTrigger trigger;
+
+    GameObject winnerScreen;
+
+    bool lastLevel;
+
+   //something audio reference
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = GetComponent<OverlapTrigger>();
+        trigger.OnTriggerEnter += EndLevel;
+    }
+
+
+
+    void EndLevel(Collider2D other)
+    {
+
+    }
+}
