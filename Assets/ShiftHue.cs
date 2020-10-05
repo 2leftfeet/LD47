1d5b6f8a295acf5aeaa12fe73add4c283b6129b8
Liutauras Gaidamavičius <liutauras@unity3d.com>
2020-10-05 21:39:55 +0300

    Easter eggu

---
+++ Assets/ShiftHue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShiftHue : MonoBehaviour
+{
+    Camera cam;
+
+    public float shiftSpeed = 0.01f;
+    float t = 0.0f;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>(); 
+    }
+
+    void Update()
+    {
+        t += Time.deltaTime * shiftSpeed;
+        if(t > 1.0f)
+        {
+            t = 0.0f;
+        }
+        cam.backgroundColor = Color.HSVToRGB(t, 0.7f, 0.7f);
+    }
+}
